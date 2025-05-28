16e17b0b37c280ab2455d9e2fbbbe4e8822e1d7f
RazanAlhasan <razanalhasan51@gmail.com>
2025-05-28 11:02:13 +0300

    user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,35 @@
 const Joi = require("joi");
 const mongoose = require("mongoose");
+const { trim } = require("validator");
 
 //user schema
+const userSchema=new  mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:5,
+        maxlength:100,
+        unique:true
+        
+    },
+    userName:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:2,
+        maxlength:200,
+        
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:6,
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+        
+    }
+},{timeseries:true , versionKey:false}) 
