d935172c09d068b158b5ddff44f4869057f59320
RazanAlhasan <razanalhasan51@gmail.com>
2025-05-29 11:47:33 +0300

    CRUD for User

--- routes/routeAuth.js
+++ routes/routeAuth.js
@@ -4,6 +4,7 @@ const router = require("./routeBook");
 const {User, validationLoginUser, 
     validationRegisterUser,
 validationUpdateUser}=require("../models/userModel");
+const jwt=require("jsonwebtoken");
 const bcrypt=require("bcryptjs");
 
 
@@ -22,18 +23,50 @@ let user=await User.findone({email:req.body.email});
 if(user){
     return res.status(400).json({message:"this user already registered"})
 }
+
+
 //hash password
 const salt=await bcrypt.genSalt(10);
 req.body.password=await bcrypt.hash(req.body.password,salt)
 
+
 user=new User({
     email:req.body.email,
     userName:req.body.userName,
-    password:req.body.password,
-    isAdmin:req.body.isAdmin
+    password:req.body.password
 });
 const result =await user.save();
-res.status(201).json(result)
+const token =user.generateToken();
+const {password,...other}=result._doc;
+res.status(201).json({...other,token})
+})
+);
+
+
+
+/**
+ * @desc Login user
+ * @route /api/auth/login
+ * @method post
+ * @access public
+ */
+router.post("/Login" , asyncHandler(async(req,res)=>{
+const {error}=validationLoginUser(req.body);
+if(error){
+    return res.status(400).json({message:error.details[0].message});
+}
+let user=await User.findone({email:req.body.email});
+if(!user){
+    return res.status(400).json({message:"invalid email or password"})
+}
+//hash password
+const isPasswordMatch=await bcrypt.compare(req.body.password,user.password)
+if(!isPasswordMatch){
+    return res.status(400).json({message:"invalid email or password"})
+}
+const token =user.generateToken();
+const {password,...other}=user._doc;
+res.status(200).json({...other,token})
 }))
 
 module.exports=router;
--- routes/routeUser.js
+++ routes/routeUser.js
@@ -1,12 +1,78 @@
 const express = require("express");
 const asyncHandler=require("express-async-handler");
-const router = express.Router(); 
-const Joi = require("joi"); 
-const Author = require("../models/authorModel");
+const router = require("./routeBook");
+const {verifyToken ,verifyTokenAndAuthorizeTheUser}=require("../middlewares/verifyToken")
+const {User,validationUpdateUser,verifyTokenAndAdmin}=require("../models/userModel");
+const bcrypt=require("bcryptjs");
 
 /**
- * @desc Get all 
- * @route /api/authors
- * @method GET
- * @access public
+ * @desc update user
+ * @route /api/users/:id
+ * @method put
+ * @access private
  */
+router.put("/:id", verifyTokenAndAuthorizeTheUser ,asyncHandler(async(req,res)=>{
+    
+    const {error}=validationUpdateUser(req.body);
+    if(error){
+    res.status(400).json({message:"invalid information"})
+}
+if(req.body.password){
+    const salt=await bcrypt.genSalt(10);
+    req.body.password=await bcrypt.hash(req.body.password, salt);
+}
+const updatedUser=await User.findByIdAndUpdate(req.params.id,{
+    $set:{
+        email:req.body.email,
+        password:req.body.password,
+        username:req.body.username,
+    }
+},{new:true}).select("-password");
+
+res.status(200).json(updatedUser);
+}))
+
+
+/**
+ * @desc get all user
+ * @route /api/users
+ * @method GET 
+ * @access private (only Admin)
+ */
+router.get("/", verifyTokenAndAdmin ,asyncHandler(async(req,res)=>{
+const users=await User.find().select("-password");
+res.status(200).json(users);
+}));
+
+/**
+ * @desc get user by id
+ * @route /api/users/:id
+ * @method GET 
+ * @access private (only Admin & user himselft)
+ */
+router.get("/:id", verifyTokenAndAuthorizeTheUser,asyncHandler(async(req,res)=>{
+const user=await User.findById(req.params.id).select("-password");
+if(user){
+    res.status(200).json(user);
+}else{
+    res.status(404).json({message:"not found this user"});
+}
+}));
+
+/**
+ * @desc delete user
+ * @route /api/users/:id
+ * @method delete
+ * @access private (only Admin & user himselft)
+ */
+router.get("/:id", verifyTokenAndAuthorizeTheUser,asyncHandler(async(req,res)=>{
+const user=await User.findById(req.params.id).select("-password");
+if(user){
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json({message:"user has been deleted "});
+}else{
+    res.status(404).json({message:"not found this user"});
+}
+}));
+
+module.exports=router;
